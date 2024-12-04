08ea7fe2c782f6e2c7f42f6c980a1610bb400365
hayathdt <hayat.haddouti2@gmail.com>
2024-12-04 15:33:23 +0100

    install tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js,css}', // Inclure tous les fichiers HTML, JS, et CSS dans le dossier src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
